490771ac2631c669937f6dd2a18f185ffe8ed1ad
Jacky Law <jacky@Jackys-MacBook-Pro.local>
2018-05-15 22:46:26 +0200

    edit

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
 <iframe class="video" width="560px" height="315px" src="https://www.youtube.com/embed/seUP34crpwc?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 
 <h3>About us</h3>
-<p>Lorem ipsum dolor sit amet, nec esse appareat cu, mollis persius has et. Ne dicunt quodsi usu, mei in amet lobortis efficiantur, cu sit mucius noluisse praesent. Te duo quodsi comprehensam signiferumque, omnis solum percipit ne eos. Ne mollis discere inciderint cum, pro no fastidii molestie. At odio diceret epicuri vix, has quando mentitum ad, ne pri possim diceret principes. An reque tractatos expetenda quo, utamur abhorreant mel no. Ne luptatum conceptam nam, ei tollit delectus constituam sed. Eam ne simul offendit quaerendum. Has at lucilius conceptam adolescens, no munere mentitum pro. Sed ad aeterno quaeque pertinax. Commune assentior cu eos, an inani mentitum laboramus vix. In mea clita adipisci, ex has fierent legendos. Ut movet necessitatibus usu. At integre platonem vim, has oblique constituam definitiones et. No eam dicam aliquid, eam zril postea complectitur id, et semper animal vis. Duo vero minim invidunt ei, altera tincidunt repudiandae eam et. Sit legere intellegat ei, noster possim ei vis. Mea at omnesque consequat voluptatum, ut dictas singulis per. Tollit necessitatibus cum in, ex fuisset partiendo reprehendunt vix, ea prima paulo vim. Summo minim insolens ius ne, nam te incorrupte theophrastus. Ut efficiantur voluptatibus his, habeo elitr et usu, amet prompta cu mea. Dicam suavitate torquatos ad usu, ut ullum possit efficiendi pri.
-
-Lorem ipsum dolor sit amet, nec esse appareat cu, mollis persius has et. Ne dicunt quodsi usu, mei in amet lobortis efficiantur, cu sit mucius noluisse praesent. Te duo quodsi comprehensam signiferumque, omnis solum percipit ne eos. Ne mollis discere inciderint cum, pro no fastidii molestie. At odio diceret epicuri vix, has quando mentitum ad, ne pri possim diceret principes. An reque tractatos expetenda quo, utamur abhorreant mel no. Ne luptatum conceptam nam, ei tollit delectus constituam sed. Eam ne simul offendit quaerendum. Has at lucilius conceptam adolescens, no munere mentitum pro. Sed ad aeterno quaeque pertinax. Commune assentior cu eos, an inani mentitum laboramus vix. In mea clita adipisci, ex has fierent legendos. Ut movet necessitatibus usu. At integre platonem vim, has oblique constituam definitiones et. No eam dicam aliquid, eam zril postea complectitur id, et semper animal vis. Duo vero minim invidunt ei, altera tincidunt repudiandae eam et. Sit legere intellegat ei, noster possim ei vis. Mea at omnesque consequat voluptatum, ut dictas singulis per. Tollit necessitatibus cum in, ex fuisset partiendo reprehendunt vix, ea prima paulo vim. Summo minim insolens ius ne, nam te incorrupte theophrastus. Ut efficiantur voluptatibus his, habeo elitr et usu, amet prompta cu mea. Dicam suavitate torquatos ad usu, ut ullum possit efficiendi pri.</p>
+<p>Lorem ipsum dolor sit amet, nec esse appareat cu, mollis persius has et. Ne dicunt quodsi usu, mei in amet lobortis efficiantur. 
+Lorem ipsum dolor sit amet, nec esse appareat cu, mollis persius has et. Ne dicunt quodsi usu, mei in amet lobortis efficiantur
+Lorem ipsum dolor sit amet, nec esse appareat cu, mollis persius has et. Ne dicunt quodsi usu, mei in amet lobortis efficiantur
+Lorem ipsum dolor sit amet, nec esse appareat cu, mollis persius has et. Ne dicunt quodsi usu, mei in amet lobortis efficiantur.</p>
 </div>
 </div>
 
@@ -16,25 +17,25 @@ Lorem ipsum dolor sit amet, nec esse appareat cu, mollis persius has et. Ne dicu
   <div class="card">
     <img class="card-img-top" src="img/header.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <h5 class="card-title">About</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted"></small></p>
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="img/header.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <h5 class="card-title">Photos</h5>
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <p class="card-text"><small class="text-muted"></small></p>
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="img/header.jpg" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <h5 class="card-title">Team</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. </p>
+      <p class="card-text"><small class="text-muted"></small></p>
     </div>
   </div>
 </div>
