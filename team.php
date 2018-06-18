61b51569d13a63c9a267085762468c8d009f4a6d
henyy <henriima@metropolia.fi>
2018-06-18 23:30:42 +0200

    edited css

--- team.php
+++ team.php
@@ -1,7 +1,7 @@
 <?php include("header_2.php"); ?>
 <div class="container">
     <h3>Team</h3>
-    <div class="row" style="margin-left: auto; margin-right: auto; width: 77%;">
+    <div class="row" style="margin-left: auto; margin-right: auto; width: 66%;">
         <div class="col" id="team"></div>
     </div>
 </div>
