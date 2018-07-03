cbdc82fa4d42617c351e822388e445f2a6ac89de
henyy <henriima@metropolia.fi>
2018-07-03 12:38:06 +0200

    edited about

--- about.php
+++ about.php
@@ -1,7 +1,9 @@
 <?php include("header_2.php"); ?>
 <div class="container">
+<div class="main">
     <h3>About</h3>
-    <p>The Hat short film is a project produced by Hochschule der Medien international students.</p>
-    <p>Lorem Impsum Blaa Blaa Blaa Lorem Impsum Blaa Blaa Blaa Lorem Impsum Blaa Blaa Blaa Lorem Impsum Blaa Blaa BlaaLorem Impsum Blaa Blaa Blaa Lorem Impsum Blaa Blaa Blaa v Lorem Impsum Blaa Blaa Blaa Lorem Impsum Blaa Blaa Blaa Lorem Impsum Blaa Blaa Blaa Lorem Impsum Blaa Blaa Blaa Lorem Impsum Blaa Blaa Blaa Lorem Impsum Blaa Blaa Blaa</p>
+    <p>The Hat short film is a project produced by the Hochschule der Medien international students. The project was ready after 5 months and without our great project group this would not have been possible. 
+    We divided our group into different departments such as camera, casting, web and post production department.</p>
+</div>
 </div>
 <?php include("footer.php");?>
